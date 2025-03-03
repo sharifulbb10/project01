ddc6f917df10745b2fb49ec4658b42c041cdce94
sharifulbb10 <sharifulbb10@gmail.com>
2025-03-03 19:49:49 +0600

    initial upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import CvComponent from './CvComponent.jsx'
+
+function App() {
+
+
+  return (
+    <>
+      <CvComponent/>
+    </>
+  )
+}
+
+export default App
---
+++ src/CvComponent.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+
+export default function CvComponent() {
+	return (
+		<div className="text-xs mt-10 ml-8 sm:text-sm ml-10">
+			<hr className="w-3/4 mb-5"/>
+			<h1 className="mb-3 text-2xl">SHARIFUL ISLAM</h1>
+			<h2 className="mb-3 text-green-600">Junior Front-end Developer</h2>
+			<p>
+				Saifullakandi <br/>
+				Bancharampur <br/>
+				Brahmanbaria, Chittagong, Bangladesh <br/>
+				(+880) 1304214979 <br/>
+				sharifulbb10@gmail.com
+			</p>
+
+			<p>
+				<h2 className="mb-2 mt-6 text-green-600">Skills</h2>
+				Html, css, javascript, react, git & github, python
+			</p>	
+
+			<p>
+				<h2 className="mb-2 mt-6 text-green-600">Education</h2>
+				<h2 className="text-blue-600">Rangpur Textile Engineering College, BuTex</h2>
+				2019 - 2025
+				<br className="mb-2"/><span className="ml-4">•&nbsp; &nbsp; Successful at solving business cases in several competitions</span>
+				<br/><span className="ml-4">•&nbsp; &nbsp; Academic scholarship winner in every semester</span>
+				<br/><span className="ml-4">•&nbsp; &nbsp; Project work: jute reinforced epoxy composite preparation and test</span>
+			</p>
+
+			<p>
+				<h2 className="mb-2 mt-6 text-green-600">Experience</h2>
+				<h2 className="text-blue-600">Libas Textile Ltd., Gazipur, Dhaka - Internship</h2>
+				June 2024 - July 2024
+				<br className="mb-2"/><span className="ml-4">•&nbsp; &nbsp; Successful at solving business cases in several competitions</span>
+				<br/><span className="ml-4">•&nbsp; &nbsp; Academic scholarship winner in every semester</span>
+				<br/><span className="ml-4">•&nbsp; &nbsp; Project work: jute reinforced epoxy composite preparation and test</span>
+			</p>
+
+			<p>
+				<h2 className="mb-2 mt-6 text-green-600">Experience</h2>
+				<h2 className="text-blue-600">Libas Textile Ltd., Gazipur, Dhaka - Internship</h2>
+				June 2024 - July 2024
+				<br className="mb-2"/><span className="ml-4">•&nbsp; &nbsp; Successful at solving business cases in several competitions</span>
+				<br/><span className="ml-4">•&nbsp; &nbsp; Academic scholarship winner in every semester</span>
+				<br/><span className="ml-4">•&nbsp; &nbsp; Project work: jute reinforced epoxy composite preparation and test</span>
+			</p>
+
+			<p className="mb-15">
+				<h2 className="mb-2 mt-6 text-green-600">Accross the Internet</h2>
+				<a href="#" className="text-blue-600">LinkedIn</a> &nbsp; &nbsp; <a href="#" className="text-blue-600">GitHub</a>
+			</p>
+		</div>
+	)
+}
